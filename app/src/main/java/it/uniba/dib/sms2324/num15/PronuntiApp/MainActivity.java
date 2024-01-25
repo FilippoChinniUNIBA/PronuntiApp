57befcaff8c607a28b2d0ace2c2e914ebd5f2078
filippochinni <filippochinni02@gmail.com>
2024-01-25 20:12:20 +0100

    Initial commit

---
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/MainActivity.java
@@ -0,0 +1,14 @@
+package it.uniba.dib.sms2324.num15.PronuntiApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //setta il layout
+    }
+}
