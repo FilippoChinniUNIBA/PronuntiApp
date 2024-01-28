f98a9730f7a578cc34db4152b69480444b429aa6
FilippoChinniUniba <filippochinniperuniba@gmail.com>
2024-01-28 15:19:32 +0100

    Update TestDBFragment.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/testingTODELETE/TestDBFragment.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/testingTODELETE/TestDBFragment.java
@@ -1,6 +1,20 @@
 package it.uniba.dib.sms2324.num15.PronuntiApp.testingTODELETE;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import it.uniba.dib.sms2324.num15.PronuntiApp.R;
+
 public class TestDBFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.test_fragment_test_db, container, false);
+
+		return view;
+	}
 }
