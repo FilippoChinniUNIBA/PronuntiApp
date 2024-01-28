d7e129f51025f80adc6c246cb310533753763819
FilippoChinniUniba <filippochinniperuniba@gmail.com>
2024-01-28 17:30:03 +0100

    Update CostantiNodiDB.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/models/database/costantidatabase/CostantiNodiDB.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/models/database/costantidatabase/CostantiNodiDB.java
@@ -1,21 +1,21 @@
 package it.uniba.dib.sms2324.num15.PronuntiApp.models.database.costantidatabase;
 
 public class CostantiNodiDB {
-	private static final String LOGOPEDISTI = "logopedisti";
-	private static final String GENITORI = "genitori";
-	private static final String PAZIENTI = "pazienti";
-	private static final String TERAPIE = "terapie";
-	private static final String TEMPLATE_SCENARIGIOCO = "templateScenariGioco";
-	private static final String SCENARIGIOCO = "scenariGioco";
-	private static final String TEMPLATE_ESERCIZI_DENOMINAZIONE_IMMAGINE = "templateEserciziDenominazioneImmagine";
-	private static final String TEMPLATE_ESERCIZI_SEQUENZA_PAROLE = "templateEserciziSequenzaParole";
-	private static final String TEMPLATE_ESERCIZI_COPPIA_IMMAGINI = "templateEserciziCoppiaImmagini";
-	private static final String ESERCIZI_DENOMINAZIONE_IMMAGINE = "eserciziDenominazioneImmagine";
-	private static final String ESERCIZI_SEQUENZA_PAROLE = "eserciziSequenzaParole";
-	private static final String ESERCIZI_COPPIA_IMMAGINI = "eserciziCoppiaImmagini";
-	private static final String RISULTATI_ESERCIZI = "risultatiEsercizi";
-	private static final String PERSONAGGI = "personaggi";
-	private static final String SBLOCCO_PERSONAGGI = "sbloccoPersonaggi";
+	public static final String LOGOPEDISTI = "logopedisti";
+	public static final String GENITORI = "genitori";
+	public static final String PAZIENTI = "pazienti";
+	public static final String TERAPIE = "terapie";
+	public static final String TEMPLATE_SCENARIGIOCO = "templateScenariGioco";
+	public static final String SCENARIGIOCO = "scenariGioco";
+	public static final String TEMPLATE_ESERCIZI_DENOMINAZIONE_IMMAGINE = "templateEserciziDenominazioneImmagine";
+	public static final String TEMPLATE_ESERCIZI_SEQUENZA_PAROLE = "templateEserciziSequenzaParole";
+	public static final String TEMPLATE_ESERCIZI_COPPIA_IMMAGINI = "templateEserciziCoppiaImmagini";
+	public static final String ESERCIZI_DENOMINAZIONE_IMMAGINE = "eserciziDenominazioneImmagine";
+	public static final String ESERCIZI_SEQUENZA_PAROLE = "eserciziSequenzaParole";
+	public static final String ESERCIZI_COPPIA_IMMAGINI = "eserciziCoppiaImmagini";
+	public static final String RISULTATI_ESERCIZI = "risultatiEsercizi";
+	public static final String PERSONAGGI = "personaggi";
+	public static final String SBLOCCO_PERSONAGGI = "sbloccoPersonaggi";
 
 	private CostantiNodiDB() {
 
