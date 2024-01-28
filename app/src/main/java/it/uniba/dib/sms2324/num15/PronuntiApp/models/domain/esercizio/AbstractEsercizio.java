6755b50541a77d22e9391c459c15a0d9bf88d258
FilippoChinniUniba <filippochinniperuniba@gmail.com>
2024-01-28 16:43:34 +0100

    Update AbstractEsercizio.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/models/domain/esercizio/AbstractEsercizio.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/models/domain/esercizio/AbstractEsercizio.java
@@ -1,12 +1,23 @@
 package it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.esercizio;
 
 public abstract class AbstractEsercizio implements Esercizio {
+	protected String idEsercizio;
 	protected int ricompensaCorretto;
 	protected int ricompensaErrato;
+
+	public AbstractEsercizio(String idEsercizio, int ricompensaCorretto, int ricompensaErrato) {
+		this.idEsercizio = idEsercizio;
+		this.ricompensaCorretto = ricompensaCorretto;
+		this.ricompensaErrato = ricompensaErrato;
+	}
 
 	public AbstractEsercizio(int ricompensaCorretto, int ricompensaErrato) {
 		this.ricompensaCorretto = ricompensaCorretto;
 		this.ricompensaErrato = ricompensaErrato;
+	}
+
+	public String getIdEsercizio() {
+		return idEsercizio;
 	}
 
 	public int getRicompensaCorretto() {
