505ae0ff8ed4956c07e3dc9f8bc5569e33aead05
FilippoChinniUniba <filippochinniperuniba@gmail.com>
2024-01-29 18:41:27 +0100

    Update costantidatabase package

---
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/models/database/costantidatabase/CostantiDBEsercizioSequenzaParole.java
@@ -0,0 +1,16 @@
+package it.uniba.dib.sms2324.num15.PronuntiApp.models.database.costantidatabase;
+
+public final class CostantiDBEsercizioSequenzaParole {
+	public static final String ID_ESERCIZIO = "idEsercizio";
+	public static final String ID_TEMPLATE_ESERCIZIO = "idTemplateEsercizio";
+	public static final String AUDIO_ESERCIZIO = "audioEsercizio";
+	public static final String PAROLA_1 = "parola1";
+	public static final String PAROLA_2 = "parola2";
+	public static final String PAROLA_3 = "parola3";
+	public static final String RICOMPENSA_CORRETTO = "ricompensaCorretto";
+	public static final String RICOMPENSA_ERRATO = "ricompensaErrato";
+	public static final String ID_SCENARIOGIOCO = "idScenarioGioco";
+
+	private CostantiDBEsercizioSequenzaParole() {}
+
+}
