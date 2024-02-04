b0b59aae8f4915c2f396c112a493c0cf9b23a464
nicolabalzano <nicolab714@gmail.com>
2024-02-04 16:02:17 +0100

    bug ambiguo package navbar di nuovo

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/testingTODELETE/TEST_Activity.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/testingTODELETE/TEST_Activity.java
@@ -4,13 +4,11 @@ import android.os.Bundle;
 
 import it.uniba.dib.sms2324.num15.PronuntiApp.R;
 import it.uniba.dib.sms2324.num15.PronuntiApp.views.activity.AbstractAppActivity;
-import it.uniba.dib.sms2324.num15.PronuntiApp.views.navigationselector.NavigationNavBarSelectorGenitore;
 
 public class TEST_Activity extends AbstractAppActivity {
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setBottomNavBar(R.menu.bottom_navbar_genitore, new NavigationNavBarSelectorGenitore(getSupportFragmentManager(), R.id.appFrameLayout, bottomNavigationView));
 		setFirstFragment(R.id.appFrameLayout, new TestMenuTestFragment());
 
 	}
