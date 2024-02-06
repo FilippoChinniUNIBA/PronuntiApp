52cab6924acc5018014fd9499f6a92a76ffd0781
Fallman1 <diegoama01gmail.com>
2024-02-06 12:01:34 +0100

    Update RegistrazionePazienteEGenitoreViewModel.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/PazientiFragment.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/PazientiFragment.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import it.uniba.dib.sms2324.num15.PronuntiApp.R;
+import it.uniba.dib.sms2324.num15.PronuntiApp.views.fragment.pazienti.RegistrazionePazienteGenitoreFragment;
 
-public class PazientiFragment extends Fragment {
+public class PazientiFragment extends AbstractFragmentWithNavigation {
     public PazientiFragment() {
     }
+
+    private Button addPazientiButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_pazienti, container, false);
+        View view = inflater.inflate(R.layout.fragment_pazienti, container, false);
+        addPazientiButton = view.findViewById(R.id.addPaziente);
+        addPazientiButton.setOnClickListener(v -> navigateTo(R.id.frameLayoutLogopedista, new RegistrazionePazienteGenitoreFragment()));
+
+        return view;
     }
 
     @Override
