7cb41ce2912bcdfcc3e841b6834959c94abf2a94
Fallman1 <diegoama01gmail.com>
2024-02-10 19:53:31 +0100

    Add AppuntamentiControllerGenitore.java e Update AppuntamentiGenitoreFragment.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/user_genitore/appuntamenti/AppuntamentiGenitoreFragment.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/user_genitore/appuntamenti/AppuntamentiGenitoreFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,14 +18,21 @@ import java.time.LocalTime;
 import java.time.temporal.TemporalAmount;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import it.uniba.dib.sms2324.num15.PronuntiApp.R;
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Appuntamento;
+import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Paziente;
+import it.uniba.dib.sms2324.num15.PronuntiApp.viewmodels.genitore_viewmodel.AppuntamentiControllerGenitore;
+import it.uniba.dib.sms2324.num15.PronuntiApp.viewmodels.genitore_viewmodel.GenitoreViewModel;
 
 public class AppuntamentiGenitoreFragment extends Fragment {
     private RecyclerView recyclerViewAppuntamentiGenitore;
     private AppuntamentoGenitoreAdapter appuntamentoAdapter;
     private List<Appuntamento> appuntamenti; // Lista degli appuntamenti (da sostituire con la tua implementazione)
+    private GenitoreViewModel mGenitoreViewModel;
+    private String idGenitore;
+    private AppuntamentiControllerGenitore mAppuntamentiControllerGenitore;
 
     public AppuntamentiGenitoreFragment() {}
 
@@ -34,6 +42,9 @@ public class AppuntamentiGenitoreFragment extends Fragment {
         recyclerViewAppuntamentiGenitore = view.findViewById(R.id.recyclerViewAppuntamentiGenitore);
         recyclerViewAppuntamentiGenitore.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        mGenitoreViewModel = new ViewModelProvider(requireActivity()).get(GenitoreViewModel.class);
+        this.idGenitore = mGenitoreViewModel.getGenitoreLiveData().getValue().getIdProfilo();
+
         return view;
     }
 
@@ -41,14 +52,15 @@ public class AppuntamentiGenitoreFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //TODO: prendere gli appuntamenti dal database
-        appuntamenti = new ArrayList<>();
-        for(int i = 0; i < 100; i++) {
-            if(i<3)
-                appuntamenti.add(new Appuntamento("idAppuntamento"+i, "refIdLogopedista"+i, "refIdGenitore"+i, LocalDate.now().minusDays(1), LocalTime.now(), "Via dei mille 10, 20123 Milano "+i));
-            else
-                appuntamenti.add(new Appuntamento("idAppuntamento"+i, "refIdLogopedista"+i, "refIdGenitore"+i, LocalDate.now(), LocalTime.now().plusHours(1), "Via dei mille 10, 20123 Milano "+i));
-        }
+
+        /*CompletableFuture<Paziente> =
+
+        Paziente paziente
+
+        CompletableFuture<List<Appuntamento>>
+
+
         appuntamentoAdapter = new AppuntamentoGenitoreAdapter(appuntamenti);
-        recyclerViewAppuntamentiGenitore.setAdapter(appuntamentoAdapter);
+        recyclerViewAppuntamentiGenitore.setAdapter(appuntamentoAdapter);*/
     }
 }
