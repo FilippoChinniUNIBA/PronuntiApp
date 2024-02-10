c10e0e16b9cb2d6e5390f77d06994119393cf39d
FilippoChinniUniba <filippochinniperuniba@gmail.com>
2024-02-10 19:23:45 +0100

    Update AbstractAppActivity.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/activity/AbstractAppActivity.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/activity/AbstractAppActivity.java
@@ -17,9 +17,14 @@ import com.google.android.material.navigation.NavigationBarView;
 import it.uniba.dib.sms2324.num15.PronuntiApp.R;
 
 public abstract class AbstractAppActivity extends AppCompatActivity {
-
     protected BottomNavigationView bottomNavigationView;
     protected NavController navcontroller;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     protected void setOnBackPressedCallback(int id) {
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
@@ -39,44 +44,6 @@ public abstract class AbstractAppActivity extends AppCompatActivity {
         });
     }
 
-    /*protected void setBottomNavBar(int menuId, NavigationNavBarItemSelector navigationNavBarItemSelector) {
-        bottomNavigationView.inflateMenu(menuId);
-        bottomNavigationView.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_LABELED);
-        bottomNavigationView.setOnItemSelectedListener(item -> navigationNavBarItemSelector.selectItem(item.getItemId()));
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-    }*/
-
-    /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Controlla se il tasto up è stato premuto
-        if (item.getItemId() == android.R.id.home) {
-            // Sostituisci il fragment corrente con il fragment precedente
-            getSupportFragmentManager().popBackStack();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    protected void setFirstFragment(int fragmentContainerId, Fragment fragment) {
-        //TODO nicola non so perché qui non setti il fragment in backstack come negli altri,
-        // comunque oltre questo, non so se ha senso usare il getSupportFragmentManager() qui
-        // forse sarebbe meglio passare il fragmentManager come attributo di classe.
-        // poi in ogni caso serve anche qui una funzione tipo replaceFragment o navigateToFragment
-        // per astrarre la transazione del fragment
-        getSupportFragmentManager().beginTransaction()
-                .replace(fragmentContainerId, fragment)
-                .commit();
-    }
-
     @Override
     public boolean onSupportNavigateUp() {
         if (navcontroller.navigateUp()) {
@@ -85,4 +52,5 @@ public abstract class AbstractAppActivity extends AppCompatActivity {
             return super.onSupportNavigateUp();
         }
     }
+
 }
