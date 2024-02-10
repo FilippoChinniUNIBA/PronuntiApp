bdb428907dcb7f1e812d8a497592a583ca69d85e
Fallman1 <diegoama01gmail.com>
2024-02-10 19:19:59 +0100

    Update AppuntamentiLogopedistaFragment.java e CreazioneAppuntamentoController.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/viewmodels/logopedista_viewmodel/LogopedistaViewModel.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/viewmodels/logopedista_viewmodel/LogopedistaViewModel.java
@@ -6,11 +6,15 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.database.profilo.LogopedistaDAO;
+import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Appuntamento;
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Logopedista;
 
 public class LogopedistaViewModel extends ViewModel {
 	private MutableLiveData<Logopedista> mLogopedistaLiveData = new MutableLiveData<>();
+	private MutableLiveData<List<Appuntamento>> mAppuntamentiLiveData = new MutableLiveData<>();
 	private RegistrazionePazienteGenitoreController mRegistrazionePazienteGenitoreController;
 	private CreazioneAppuntamentoController mCreazioneAppuntamentoController;
 
@@ -21,6 +25,13 @@ public class LogopedistaViewModel extends ViewModel {
 		mLogopedistaLiveData.setValue(logopedista);
 	}
 
+	public LiveData<List<Appuntamento>> getmAppuntamentiLiveData() {
+		return mAppuntamentiLiveData;
+	}
+
+	public void setAppuntamenti(List<Appuntamento> appuntamenti) {
+		this.mAppuntamentiLiveData.setValue(appuntamenti);
+	}
 
 	public void aggiornaLogopedistaRemoto() {
 		Logopedista logopedista = mLogopedistaLiveData.getValue();
