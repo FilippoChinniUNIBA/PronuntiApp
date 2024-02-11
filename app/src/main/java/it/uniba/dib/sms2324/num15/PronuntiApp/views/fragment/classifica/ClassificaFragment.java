cbdce49bbc4cfbc6860739eae5256e6b29a44960
Fallman1 <diegoama01gmail.com>
2024-02-11 18:26:01 +0100

    Update ClassificaFragment.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/classifica/ClassificaFragment.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/classifica/ClassificaFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,9 +18,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import it.uniba.dib.sms2324.num15.PronuntiApp.R;
+import it.uniba.dib.sms2324.num15.PronuntiApp.models.database.profilo.personaggio.PersonaggioDAO;
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Paziente;
+import it.uniba.dib.sms2324.num15.PronuntiApp.viewmodels.paziente_viewmodels.ClassificaPazienteController;
+import it.uniba.dib.sms2324.num15.PronuntiApp.viewmodels.paziente_viewmodels.PazienteViewModel;
 import it.uniba.dib.sms2324.num15.PronuntiApp.views.activity.AbstractAppActivity;
 import it.uniba.dib.sms2324.num15.PronuntiApp.views.dialog.ConnessioneErroreDialog;
 import it.uniba.dib.sms2324.num15.PronuntiApp.views.fragment.AbstractFragmentWithNavigation;
@@ -28,6 +33,8 @@ public class ClassificaFragment extends AbstractFragmentWithNavigation {
     private RecyclerView recyclerViewClassifica;
     private PazienteClassificaAdapter pazienteClassificaAdapter;
     private List<PazienteClassifica> pazienti;
+    private PazienteViewModel mPazienteViewModel;
+    private ClassificaPazienteController mClassificaPazienteController;
 
     private final String url_img="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ilgiornale.it%2Fnews%2Fspettacoli%2Fjoker-grande-film-e-i-pericoli-dellempatia-1763658.html&psig=AOvVaw20KYZ33AVj0UJ1zQVFhpbS&ust=1707670729411000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCg1aufoYQDFQAAAAAdAAAAABAJ";
 
@@ -36,6 +43,9 @@ public class ClassificaFragment extends AbstractFragmentWithNavigation {
         View view = inflater.inflate(R.layout.fragment_classifica, container, false);
         recyclerViewClassifica = view.findViewById(R.id.recyclerViewClassifica);
         recyclerViewClassifica.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        this.mPazienteViewModel = new ViewModelProvider(requireActivity()).get(PazienteViewModel.class);
+        this.mClassificaPazienteController = mPazienteViewModel.getClassificaPazienteController();
 
         return view;
     }
@@ -47,6 +57,27 @@ public class ClassificaFragment extends AbstractFragmentWithNavigation {
         //TODO: costruire la classifica creando una lista di tipo PazienteClassifica (username, punteggio, url_img)
         // url_img si ottiene da personaggioDao ottenendo prima l'id del personaggio selezionato da Paziente paziente.getPersonaggiSbloccati() e
         // poi cercare la chiave con il valore 2 (che significa selezionato)
+        PersonaggioDAO personaggioDAO = new PersonaggioDAO();
+
+/*
+        String idPaziente = mPazienteViewModel.getPazienteLiveData().getValue().getIdProfilo();
+        List<PazienteClassifica> pazientiClassifica = null;
+        mClassificaPazienteController.retrieveClassificaPazienti(idPaziente).thenAccept(logopedista -> {
+            for (Paziente paziente : logopedista.getPazienti()) {
+                    String idPersonaggio = paziente.getPersonaggiSbloccati().entrySet().stream()
+                            .filter(entry -> entry.getValue().equals(2)).findFirst()
+                            .map(Map.Entry::getKey);
+                });
+                personaggioDAO.get();
+                pazientiClassifica.add(paziente.getUsername(),paziente.getPunteggioTot(),);
+            }
+
+        });*/
+
+
+
+
+
         pazienti= new ArrayList<>();
         for(int i=100; i>0; i--){
             pazienti.add(new PazienteClassifica("username_paziente"+i, i, url_img));
