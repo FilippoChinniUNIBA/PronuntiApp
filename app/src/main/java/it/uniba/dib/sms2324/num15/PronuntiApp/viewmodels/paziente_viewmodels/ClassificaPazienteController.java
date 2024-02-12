47815f459ce093859ac462033d3f52df9ad5e93a
Fallman1 <diegoama01gmail.com>
2024-02-12 11:31:53 +0100

    Update ClassificaFragment.java

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/viewmodels/paziente_viewmodels/ClassificaPazienteController.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/viewmodels/paziente_viewmodels/ClassificaPazienteController.java
@@ -2,21 +2,43 @@ package it.uniba.dib.sms2324.num15.PronuntiApp.viewmodels.paziente_viewmodels;
 
 import org.checkerframework.checker.units.qual.C;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.database.profilo.PazienteDAO;
+import it.uniba.dib.sms2324.num15.PronuntiApp.models.database.profilo.personaggio.PersonaggioDAO;
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Logopedista;
+import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Paziente;
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.classifica.Classifica;
+import it.uniba.dib.sms2324.num15.PronuntiApp.views.fragment.classifica.PazienteClassifica;
 
 public class ClassificaPazienteController {
-/*
-    public CompletableFuture<Logopedista> retrieveClassificaPazienti(String idPaziente){
+
+    public List<PazienteClassifica> retrieveClassificaPazienti(String idPaziente){
 
         PazienteDAO pazienteDAO = new PazienteDAO();
-        CompletableFuture<Classifica> future = new CompletableFuture<>();
+        PersonaggioDAO personaggioDAO = new PersonaggioDAO();
+
+        List<PazienteClassifica> pazientiClassifica = new ArrayList<>();
         pazienteDAO.getDatiLogopedistaByIdPaziente(idPaziente).thenAccept(logopedista -> {
-
+            for (Paziente paziente : logopedista.getPazienti()) {
+                for (Map.Entry<String, Integer> entry : paziente.getPersonaggiSbloccati().entrySet()) {
+                    Integer valore = entry.getValue();
+                    if (valore == 2) {
+                        String chiave = entry.getKey();
+                        System.out.println("Chiave: " + chiave + ", Valore: " + valore);
+                        personaggioDAO.getById(chiave).thenAccept(personaggio -> {
+                            PazienteClassifica pazienteClassifica = new PazienteClassifica(paziente.getUsername(), paziente.getPunteggioTot(), personaggio.getTexturePersonaggio());
+                            pazientiClassifica.add(pazienteClassifica);
+                        });
+                        break;
+                    }
+                }
+            }
         });
-
-    }*/
+        pazientiClassifica.sort((p1,p2)-> p2.getPunteggio() - p1.getPunteggio());
+        return pazientiClassifica;
+    }
 }
--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/classifica/ClassificaFragment.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/classifica/ClassificaFragment.java
@@ -8,15 +8,19 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.uniba.dib.sms2324.num15.PronuntiApp.R;
+import it.uniba.dib.sms2324.num15.PronuntiApp.models.database.costantidatabase.CostantiDBPersonaggio;
 import it.uniba.dib.sms2324.num15.PronuntiApp.models.database.profilo.personaggio.PersonaggioDAO;
+import it.uniba.dib.sms2324.num15.PronuntiApp.models.domain.profilo.Paziente;
 import it.uniba.dib.sms2324.num15.PronuntiApp.viewmodels.paziente_viewmodels.ClassificaPazienteController;
 import it.uniba.dib.sms2324.num15.PronuntiApp.viewmodels.paziente_viewmodels.PazienteViewModel;
 import it.uniba.dib.sms2324.num15.PronuntiApp.views.fragment.AbstractFragmentWithNavigation;
@@ -37,7 +41,7 @@ public class ClassificaFragment extends AbstractFragmentWithNavigation {
         recyclerViewClassifica.setLayoutManager(new LinearLayoutManager(getContext()));
 
         this.mPazienteViewModel = new ViewModelProvider(requireActivity()).get(PazienteViewModel.class);
-        //this.mClassificaPazienteController = mPazienteViewModel.getClassificaPazienteController();
+        this.mClassificaPazienteController = mPazienteViewModel.getClassificaPazienteController();
 
         return view;
     }
@@ -49,35 +53,11 @@ public class ClassificaFragment extends AbstractFragmentWithNavigation {
         //TODO: costruire la classifica creando una lista di tipo PazienteClassifica (username, punteggio, url_img)
         // url_img si ottiene da personaggioDao ottenendo prima l'id del personaggio selezionato da Paziente paziente.getPersonaggiSbloccati() e
         // poi cercare la chiave con il valore 2 (che significa selezionato)
-        PersonaggioDAO personaggioDAO = new PersonaggioDAO();
 
-/*
         String idPaziente = mPazienteViewModel.getPazienteLiveData().getValue().getIdProfilo();
-        List<PazienteClassifica> pazientiClassifica = null;
-        mClassificaPazienteController.retrieveClassificaPazienti(idPaziente).thenAccept(logopedista -> {
-            for (Paziente paziente : logopedista.getPazienti()) {
-                    String idPersonaggio = paziente.getPersonaggiSbloccati().entrySet().stream()
-                            .filter(entry -> entry.getValue().equals(2)).findFirst()
-                            .map(Map.Entry::getKey);
-                });
-                personaggioDAO.get();
-                pazientiClassifica.add(paziente.getUsername(),paziente.getPunteggioTot(),);
-            }
+        Log.d("ClassificaFragment","mPazienteViewModel.getPazienteLiveData().getValue().getIdProfilo()"+mPazienteViewModel.getPazienteLiveData().getValue().getIdProfilo());
+            pazienteClassificaAdapter = new PazienteClassificaAdapter(mClassificaPazienteController.retrieveClassificaPazienti(idPaziente),idPaziente);
+            recyclerViewClassifica.setAdapter(pazienteClassificaAdapter);
 
-        });*/
-
-
-
-
-
-        pazienti= new ArrayList<>();
-        for(int i=100; i>0; i--){
-            pazienti.add(new PazienteClassifica("username_paziente"+i, i, url_img));
-        }
-        pazienti.sort((p1, p2) -> p2.getPunteggio() - p1.getPunteggio());
-
-        //TODO: creare l'adapter passandor come secondo parametro l'username del paziente attuale
-        pazienteClassificaAdapter = new PazienteClassificaAdapter(pazienti, pazienti.get(5).getUsername());
-        recyclerViewClassifica.setAdapter(pazienteClassificaAdapter);
     }
 }
