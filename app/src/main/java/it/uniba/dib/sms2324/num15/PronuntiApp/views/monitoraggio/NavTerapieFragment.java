b73b2bf1487d99343c8cccff13fc3b8565f95f3b
nicolabalzano <nicolab714@gmail.com>
2024-02-15 19:06:04 +0100

    fix revert font on esercizi

---
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/monitoraggio/NavTerapieFragment.java
@@ -0,0 +1,46 @@
+package it.uniba.dib.sms2324.num15.PronuntiApp.views.monitoraggio;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import it.uniba.dib.sms2324.num15.PronuntiApp.R;
+import it.uniba.dib.sms2324.num15.PronuntiApp.views.fragment.AbstractFragmentWithNavigation;
+
+public class NavTerapieFragment extends AbstractFragmentWithNavigation {
+
+    private ImageButton imageButtonProssimaTerapia;
+    private ImageButton imageButtonTerapiaPrecedente;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_nav_terapie, container, false);
+        imageButtonProssimaTerapia = view.findViewById(R.id.buttonAvantiTerapia);
+        imageButtonTerapiaPrecedente = view.findViewById(R.id.buttonIndietroTerapia);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imageButtonProssimaTerapia.setOnClickListener(v -> {
+            //TODO implementare funzionalità per passare alla terapia successiva
+        });
+
+        imageButtonTerapiaPrecedente.setOnClickListener(v -> {
+            //TODO implementare funzionalità per passare alla terapia precedente
+        });
+
+    }
+
+}
---
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/monitoraggio/TerapieFragment.java
@@ -0,0 +1,30 @@
+package it.uniba.dib.sms2324.num15.PronuntiApp.views.monitoraggio;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import it.uniba.dib.sms2324.num15.PronuntiApp.R;
+import it.uniba.dib.sms2324.num15.PronuntiApp.views.fragment.AbstractFragmentWithNavigation;
+
+public class TerapieFragment extends AbstractFragmentWithNavigation {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+         super.onCreateView(inflater, container, savedInstanceState);
+         //View view = inflater.inflate(R.layout., container, false);
+
+         return null;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
