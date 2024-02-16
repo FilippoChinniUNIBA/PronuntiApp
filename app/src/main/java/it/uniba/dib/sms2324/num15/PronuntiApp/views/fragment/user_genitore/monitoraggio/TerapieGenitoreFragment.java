5f296b75a975c2acecd07e585d981e17ccf8a39d
nicolabalzano <nicolab714@gmail.com>
2024-02-16 11:15:05 +0100

    update monitoraggio genitore

--- app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/user_genitore/monitoraggio/TerapieGenitoreFragment.java
+++ app/src/main/java/it/uniba/dib/sms2324/num15/PronuntiApp/views/fragment/user_genitore/monitoraggio/TerapieGenitoreFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentContainerView;
 
 import it.uniba.dib.sms2324.num15.PronuntiApp.R;
 import it.uniba.dib.sms2324.num15.PronuntiApp.views.fragment.AbstractFragmentWithNavigation;
 
 public class TerapieGenitoreFragment extends AbstractFragmentWithNavigation {
 
+    private FragmentContainerView fragmentContainerViewMonitoraggio;
 
     @Nullable
     @Override
@@ -20,9 +24,10 @@ public class TerapieGenitoreFragment extends AbstractFragmentWithNavigation {
          super.onCreateView(inflater, container, savedInstanceState);
          View view = inflater.inflate(R.layout.fragment_terapie, container, false);
 
-         ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+        fragmentContainerViewMonitoraggio = view.findViewById(R.id.fragmentContainerViewMonitoraggio);
+
+         FragmentContainerView.MarginLayoutParams params = (FragmentContainerView.MarginLayoutParams) fragmentContainerViewMonitoraggio.getLayoutParams();
          params.setMargins(0, 0, 0, getContext().getResources().getDimensionPixelSize(R.dimen.nav_bar_height));
-         view.findViewById(R.id.fragmentContainerViewMonitoraggio).setLayoutParams(params);
 
          getChildFragmentManager().beginTransaction().replace(R.id.fragmentContainerViewMonitoraggio, new MonitoraggioGenitoreFragment()).commit();
 
